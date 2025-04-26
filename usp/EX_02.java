34cbfcae01480deb1137241b8b286cb8a03e0ce0
MatheusUlisses1 <matheusulissesamaraldosreis@gmail.com>
2025-04-26 11:33:24 -0300

    Add files via upload

---
+++ usp/EX_02.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        double precolar = 5.00;
+        double precobanana = 3.00;
+        double soma = precolar + precobanana;
+        double desc = soma * 0.80;
+        System.out.println("Uma laranja custa R$" + precolar + ", e uma banana custa R$" + precobanana + ", a soma da laranja e da banana é de R$" + soma + ", com o desconto de 80% fica R$" + desc);
+    }
+}
