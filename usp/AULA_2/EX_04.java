8c9136bda670b64ea1b9f0369d01b3df350f7f8f
MatheusUlisses1 <matheusulissesamaraldosreis@gmail.com>
2025-05-24 11:03:29 -0300

    Update EX_04.java

--- usp/AULA_2/EX_04.java
+++ usp/AULA_2/EX_04.java
@@ -3,9 +3,7 @@ public class EX_04
 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		
 		int N;
-		
 		System.out.println("Escreva um número: ");
 		N = input.nextInt();
 		for (int i = 0; i <= N; i++) {
