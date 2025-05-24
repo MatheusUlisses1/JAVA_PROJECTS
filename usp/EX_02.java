ef7044d787ea6674f956f6a1b24dbe18f7265bda
MatheusUlisses1 <matheusulissesamaraldosreis@gmail.com>
2025-05-24 10:12:00 -0300

    Add files via upload

---
+++ usp/EX_02.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class EX_02
+{
+	public static void main(String[] args) {
+		int x = 0;
+		int y = 0;
+		while (y <= 100) {
+		    x = x + y;
+		    y++;
+		    System.out.println(x);
+		}
+		
+	}
+}
