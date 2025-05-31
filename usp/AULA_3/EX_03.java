c8935b30376ca7504be0c44be6017be6cda02cc7
MatheusUlisses1 <matheusulissesamaraldosreis@gmail.com>
2025-05-31 11:12:31 -0300

    Update EX_03.java

--- usp/AULA_3/EX_03.java
+++ usp/AULA_3/EX_03.java
@@ -3,11 +3,12 @@ public class EX_03
 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		System.out.print("Insira o tamanho do array:");
+		System.out.print("Insira o tamanho do array: ");
 		int N = input.nextInt();
 		int[] num = new int[N];
 		for (int i = 0; i < N; i++) {
-		    System.out.print("Insira o numera na " + i + "º posição: ");
+		    int cont = i + 1;
+		    System.out.print("Insira o numera na " + cont + "º posição: ");
 		    int num1 = input.nextInt();
 		    num[i] = num1;
 		}
