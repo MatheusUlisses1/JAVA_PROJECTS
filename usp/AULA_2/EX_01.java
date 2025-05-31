0e6f8c61817d5db3ab51a077e3204e6adf20f6dc
MatheusUlisses1 <matheusulissesamaraldosreis@gmail.com>
2025-05-31 10:19:39 -0300

    Add files via upload

--- usp/AULA_2/EX_01.java
+++ usp/AULA_2/EX_01.java
@@ -3,12 +3,17 @@ public class EX_01
 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		int x = 0;
-		System.out.println("Escreva uma palavra:");
-		String palavra = input.nextLine();
-		while (x < 5) {
-		    System.out.println(palavra);
-		    x++;
-		}
+		int[] num = new int[3];
+		System.out.print("Insira o primeiro numero: ");
+		int N1 = input.nextInt();
+		num[0] = N1;
+		System.out.print("Insira o segundo numero: ");
+		int N2 = input.nextInt();
+		num[1] = N2;
+		System.out.print("Insira o terceiro numero: ");
+		int N3 = input.nextInt();
+		num[2] = N3;
+		int soma = num[0] + num[1] + num[2];
+		System.out.println("A soma total é: " + soma);
 	}
 }
