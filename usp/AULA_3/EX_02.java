8d3154d43cad75b8eb1b33453c62936c91c33f74
MatheusUlisses1 <matheusulissesamaraldosreis@gmail.com>
2025-05-31 10:52:02 -0300

    Add files via upload

---
+++ usp/AULA_3/EX_02.java
@@ -0,0 +1,7 @@
+public class EX_02
+{
+	public static void main(String[] args) {
+		String[] nomes = {"Ana", "Beatriz", "Carlos", "Danilo", "Erick"};
+		System.out.println(nomes.length + " " + nomes[0]);
+	}
+}
